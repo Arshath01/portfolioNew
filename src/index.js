3aac593868f76f311d798180716bd007f639ab0d
Arshath Ahamed <ahamedarshath35@gmail.com>
2023-05-06 21:48:15 +0530

    bootstrap added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
-import 'D:/Bootstrap/node_modules/bootstrap/dist/css/bootstrap.css'
+import './styles/Bootstrap/node_modules/bootstrap/dist/css/bootstrap.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
